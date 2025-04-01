fa7ca645d00ca76d4947969f9556b874fa7a8508
Đào Minh Thuấn <53705853+daominhthuan42@users.noreply.github.com>
2025-04-01 23:31:00 +0700

    [MINDX] BUOI 2

---
+++ 00_MODULE_1/BUOI_2/BTVN_L2_DAO_MINH_THUAN.sql
@@ -0,0 +1,99 @@
+﻿USE D4E_LESSION2_HOMEWORK
+GO
+
+/*
+BÀI TẬP 1
+Phần 1: 
+Dựa vào file mô tả CSDL mà chính bạn đã xây dựng, hãy xác định khoá chính, khoá ngoại cho các bảng trong CSDL của công ty MX ,
+sau khi xác định xong, hãy bổ sung các thông tin về khoá chính , 
+khóa ngoại và tham chiếu của chúng tới từng bảng trong file mô tả CSDL.
+
+Phần 2: 
+Bạn hãy thực hiện bổ sung khoá chính - khoá ngoại và các tham chiếu trong CSDL với SQL dựa vào bản mô tả CSDL mà bạn vừa hoàn thành ở phần 1.
+
+Phần 3: 
+Trong bảng HOADON, nếu xóa cột OrderLine, khi đó, trường thông tin nào sẽ làm khóa chính cho bảng?
+*/
+
+-- PHẦN 1
+-- VỚI BẢNG [MX_HOADON]
+-- KHÓA CHÍNH: [OrderLine]
+-- KHÓA NGOẠI: [SalesID], [ProductId]
+
+-- VỚI BẢNG [MX_NHANVIEN]
+-- KHÓA CHÍNH: [EmID]
+-- THAM CHIẾU ĐẾN [SalesID] CỦA BẢNG [MX_HOADON].
+
+-- VỚI BẢNG [MX_SANPHAM]
+-- KHÓA CHÍNH: [ProductId]
+-- THAM CHIẾU ĐẾN [ProductID] BẢNG [MX_HOADON]
+
+-- PHẦN 2
+-- [MX_HOADON]
+ALTER TABLE [dbo].[MX_HOADON] ADD PRIMARY KEY([OrderLine]);
+
+-- TẠO KHÓA NGOẠI CHO ProductId
+ALTER TABLE [dbo].[MX_HOADON] ADD FOREIGN KEY([ProductID]) REFERENCES [dbo].[MX_SANPHAM]([ProductId]);
+
+-- TẠO KHÓA NGOẠI CHO SalesID
+ALTER TABLE [dbo].[MX_HOADON]
+ADD FOREIGN KEY([SalesID]) REFERENCES [dbo].[MX_NHANVIEN]([EmID]);
+
+-- PHẦN 3:
+-- Nếu xóa cột OrderLine thì ta sẽ dùng 2 coloumn [OrderID] và [CusID] làm khóa chính cho bảng.
+
+/*
+Áp dụng các toán tử so sánh để xây dựng các câu truy vấn phục vụ cho việc chuẩn bị dữ liệu
+Đề bài : Anh Lê Đức Hiếu, giám đốc bán hàng của công ty đề xuất sẽ tung ra 1 chiến dịch truyền thông giảm giá 10% 
+cho tất cả hoá đơn mua hàng vào ngày 3/4/2022 (MM/DD/YYYY). Do vậy anh muốn bạn hãy lấy ra các đơn hàng mua hàng trước ngày 3/4/2022
+để có số liệu đối chiếu xem chiến dịch truyền thông giảm giá có ảnh hưởng đến quyết định mua sắm. 
+Bạn hãy chuẩn bị dữ liệu bằng cách lấy ra các đơn hàng có ngày lập hoá đơn trước ngày 3/4/2022
+*/
+SELECT OrderID
+FROM [dbo].[MX_HOADON]
+WHERE DateCreate < '2022-03-04';
+
+/*
+Bài tập 3: Áp dụng các toán tử logic để xây dựng các câu truy vấn phục vụ cho việc chuẩn bị dữ liệu
+Bài tập 3.1:
+Đề bài : Hiện tại các tỉnh miền Trung đang bị ảnh hưởng chung bởi bão, lũ. Nhất là 2 tỉnh Quảng Bình và Quảng Trị.
+BOD quyết định sẽ hỗ trợ cho các nhân viên có quê quán ở 2 tỉnh trên. 
+Bạn hãy viết câu lệnh truy vấn để tìm ra các nhân viên có quê quán từ 2 tỉnh trên.
+
+Bài tập 3.2:
+Đề bài: Do ảnh hưởng của bão, lũ. Công ty quyết định cho các nhân viên có quê ở các tỉnh miền Trung bị ảnh hưởng được tạm nghỉ về nhà để giúp đỡ gia đình phòng chống bão, lũ.
+Bạn hãy chuẩn bị cho BOD 2 danh sách nhân viên gồm: 
+- Danh sách các nhân sự ở miền Trung được tạm nghỉ về quê
+- Danh sách các nhân sự ở lại làm việc.
+Biết rằng, các tỉnh miền Trung bị ảnh hưởng bởi bão, lũ gồm các tỉnh Nghệ An, Hà Tĩnh, Quảng Bình, Quảng Trị và Thừa Thiên Huế.
+*/
+-- 3.1
+SELECT *
+FROM [dbo].[MX_NHANVIEN]
+WHERE [Address] IN (N'Quảng Bình', N'Quảng Trị');
+
+-- 3.2
+-- Danh sách các nhân sự ở miền Trung được tạm nghỉ về quê
+SELECT *
+FROM [dbo].[MX_NHANVIEN]
+WHERE [Address] IN (N'Nghệ An', N'Hà Tĩnh', N'Thừa Thiên Huế', N'Quảng Bình', N'Quảng Trị');
+
+-- Danh sách các nhân sự ở lại làm việc.
+SELECT *
+FROM [dbo].[MX_NHANVIEN]
+WHERE [Address] NOT IN (N'Nghệ An', N'Hà Tĩnh', N'Thừa Thiên Huế', N'Quảng Bình', N'Quảng Trị');
+
+/*
+Bài tập 4: Áp dụng các hàm để xác định dữ liệu NULL và xử lý dữ liệu NULL
+Đề bài : Do lỗi trong quá trình nhập liệu nên có một số nhân viên có ngày onboarding bị để trống.
+BOD muốn bạn tìm ra các nhân viên đó và thay thế ngày onboarding từ NULL thành ngày 1/1/2020. 
+*/
+-- Tìm ra các nhân viên có StartDate là NULL
+SELECT *
+FROM [dbo].[MX_NHANVIEN]
+WHERE [StartDate] IS NULL;
+
+-- Thay thế ngày onboarding từ NULL thành ngày 1/1/2020.
+UPDATE [dbo].[MX_NHANVIEN]
+SET [StartDate] = '2020-01-01'
+WHERE [StartDate] IS NULL;
---
+++ 00_MODULE_1/BUOI_2/LESSON_2.sql
@@ -0,0 +1,148 @@
+﻿CREATE DATABASE D4E_LESSION2_HOMEWORK
+GO
+
+USE D4E_LESSION2_HOMEWORK
+GO
+
+CREATE TABLE STUDENTS (
+	sID CHAR(5),
+	sFirstName VARCHAR(10),
+	sLastName VARCHAR(10),
+	sPhone CHAR(10),
+	sAddress VARCHAR(50)
+);
+
+CREATE TABLE COURSE (
+	cID CHAR(5),
+	cMajor VARCHAR(10),
+	cName VARCHAR(30)
+);
+
+CREATE TABLE TEACHER (
+	tID CHAR(5),
+	tFirstName VARCHAR(10),
+	tLastName VARCHAR(10),
+	tPhone CHAR(10),
+	tType INT,
+	tMajor VARCHAR(10)
+);
+
+CREATE TABLE ENROLLMENTS (
+	sID CHAR(5),
+	cID VARCHAR(10),
+	tID CHAR(5),
+);
+
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0001','MC001','T0005');
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0002','MC002','T0006');
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0003','MC003','T0006');
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0004','MC004','T0002');
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0005','MC005',NULL);
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0003','MC001','T0005');
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0006','MC002','T0006');
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0002','MC003','T0006');
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0002','MC004','T0002');
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0006','MC005','T0003');
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0004','MC001','T0005');
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0001','MC002','T0006');
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0006','MC003',NULL);
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0006','MC004','T0002');
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0002','MC005','T0003');
+INSERT INTO ENROLLMENTS(sID,cID,tID) VALUES ('M0002','MC006',NULL);
+
+--DROP TABLE [dbo].[COURSE];
+
+INSERT INTO [dbo].[STUDENTS]
+VALUES
+('M0001', 'Minh', 'Nguyen', '0323456789', 'Quang Binh'),
+('M0002', 'Hai', 'Do', '0143456789', 'Ha Noi'),
+('M0003', 'Bao', 'Nguyen', '0123656789', 'Quang Binh'),
+('M0004', 'Thuan', 'Tran', '0123456289', 'Sai Gon'),
+('M0005', 'Thao', 'Doan', '0223456589', 'Sai Gon'),
+('M0006', 'Giau', 'Le', '0723456459', 'Binh Phuoc'),
+('M0007', 'Khoa', 'Tran', '0343452780', 'Dong Nai');
+
+INSERT INTO [dbo].[COURSE]
+VALUES
+('MC001', 'Data', 'D4E'),
+('MC002', 'Data', 'BI'),
+('MC003', 'Data', 'BE BASIC'),
+('MC004', 'Web', 'WEB BASIC'),
+('MC005', 'Web', 'FullStack'),
+('MC006', 'Finance', 'FM Basic'),
+('MC007', 'Finance', 'FM Intensive');
+
+INSERT INTO [dbo].[TEACHER]
+VALUES
+('T0001', 'Bao', 'Nguyen', '0233456789', 1, 'Finance'),
+('T0002', 'Duy', 'Vo', '0233456789', 1, 'Web'),
+('T0003', 'Khoa', 'Dao', '0113656789', 0, 'Web'),
+('T0004', 'Phuoc', 'Nguyen', '0347456289', 0, 'Finance'),
+('T0005', 'Nghia', 'Cao', '0562456590', 0, 'Data'),
+('T0006', 'Ha', 'San', '0783456459', 1, 'Data');
+
+/*
+Bạn hãy xác định khoá chính, khóa ngoại cho các bảng và tiến hành thêm các ràng buộc dữ liệu giữa các bảng theo như thông tin tham chiếu trong dữ liệu mẫu
+*/
+-- TẠO KHÓA CHÍNH CHO CÁC BẢNG STUDENTS, TEACHER, COURSE
+-- COURSE
+ALTER TABLE [dbo].[COURSE]
+ALTER COLUMN cID CHAR(5) NOT NULL
+ALTER TABLE [dbo].[COURSE] ADD PRIMARY KEY([cID]);
+
+-- STUDENTS
+ALTER TABLE [dbo].[STUDENTS]
+ALTER COLUMN sID CHAR(5) NOT NULL 
+ALTER TABLE [dbo].[STUDENTS] ADD PRIMARY KEY([sID]);
+
+-- TEACHER
+ALTER TABLE [dbo].[TEACHER]
+ALTER COLUMN tID CHAR(5) NOT NULL
+ALTER TABLE [dbo].[TEACHER] ADD PRIMARY KEY(tID);
+
+-- TẠO KHÓA NGOẠI ENROLLMENTS
+ALTER TABLE [dbo].[ENROLLMENTS]
+ALTER COLUMN cID CHAR(5)
+
+ALTER TABLE [dbo].[ENROLLMENTS]
+ADD CONSTRAINT FK_ENROLLMENTS FOREIGN KEY(cID) REFERENCES [dbo].[COURSE](cID)
+
+ALTER TABLE [dbo].[ENROLLMENTS]
+ADD CONSTRAINT FK_ENROLLMENTS_STUDENT FOREIGN KEY(sID) REFERENCES [dbo].[STUDENTS](sID)
+
+ALTER TABLE [dbo].[ENROLLMENTS]
+ADD CONSTRAINT FK_ENROLLMENTS_TEACHER FOREIGN KEY(tID) REFERENCES [dbo].[TEACHER](tID)
+
+-- Bạn hãy tìm ra những giảng viên là Super Teacher của khoa Data
+SELECT T.*
+FROM [dbo].[TEACHER] AS T
+WHERE T.tType = 1 AND T.tMajor LIKE 'Data';
+
+--Bạn hãy tìm ra những học viên nào học cùng môn MC005, được dạy bởi giáo viên T0003
+SELECT S.*
+FROM [dbo].[STUDENTS] AS S, [dbo].[ENROLLMENTS] AS E, [dbo].[TEACHER] AS T, [dbo].[COURSE] AS C
+WHERE  C.cID LIKE 'MC005' AND 
+	   T.tID LIKE 'T0003' AND 
+	   S.sID = E.sID AND 
+	   C.cID = E.cID AND 
+	   T.tID = E.tID;
+
+-- Bạn hãy tìm những học viên nào đăng ký các môn học thuộc khoa Data
+SELECT DISTINCT S.*
+FROM [dbo].[STUDENTS] AS S, [dbo].[COURSE] AS C, [dbo].[ENROLLMENTS] AS E
+WHERE E.cID = C.cID AND
+	  E.sID = S.sID AND
+	  C.cMajor LIKE 'Data';
+
+--Trong bảng ENROLLMENTS, có 1 số dữ liệu bị NULL ở cột tID, 
+--bạn hãy tìm ra chúng và thay thế lại thành giáo viên có mã GV là T0003
+SELECT *, ISNULL(tID, 'T0003')
+FROM [dbo].[ENROLLMENTS]
+
+UPDATE [dbo].[ENROLLMENTS]
+SET tID = 'T0003'
+WHERE tID is NULL;
+
+--UPDATE [dbo].[ENROLLMENTS]
+--SET tID = ISNULL(tID, 'T0003');
+
